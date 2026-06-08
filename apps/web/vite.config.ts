b3be29e248ef58869af824a47d6a889bca7f29fe
Mateusz Tylec <mateusztylec@duck.com>
2026-06-08 15:50:31 +0200

    fix: deploy

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
     port: process.env.PORT ? Number(process.env.PORT) : 5173,
     proxy: {
-      "/api": { target: "http://localhost:3000", changeOrigin: true },
+      "/api": { target: "http://localhost:3000" },
     },
   },
 });
